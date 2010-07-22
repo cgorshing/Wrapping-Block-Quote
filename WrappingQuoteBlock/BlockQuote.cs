1b5428b8effb7a17eb536889d2045329165ceaea
Chad Gorshing <gorshing@gmail.com>
2010-07-21 21:29:21 -0500

    Initial project import

---
+++ WrappingQuoteBlock/BlockQuote.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using ScrewTurn.Wiki.PluginFramework;
+
+namespace BlockQuoteWithFormatting
+{
+    /// <summary>
+    /// So much of this class was stolen from
+    /// Greenicicle ScrewTurn Syntax Highlighter
+    /// http://code.google.com/p/greeniciclescrewturnsyntaxhighlighter/source/browse/trunk/%20greeniciclescrewturnsyntaxhighlighter/src/SyntaxHighlightPlugin/SyntaxHighlightFormatProvider.cs
+    /// I give them much props - thank you.
+    /// </summary>
+	public class BlockQuote : IFormatterProviderV30
+	{
+		private IHostV30 _host;
+		private string _config;
+		private bool _enableLogging;
+
+		public bool PerformPhase1
+		{
+			get { return false; }
+		}
+
+		public bool PerformPhase2
+		{
+			get { return true; }
+		}
+
+		public bool PerformPhase3
+		{
+			get { return false; }
+		}
+
+		public int ExecutionPriority
+		{
+			get { return 50; }
+		}
+
+		public string Format(string raw, ContextInformation context, FormattingPhase phase)
+		{
+            if (context.Context != FormattingContext.PageContent) return raw;
+
+            var targetText = new StringBuilder();
+		    string openingTag = "{quote}";
+            string closingTag = "{/quote}";
+            var pattern = new Regex(openingTag + ".*?" + closingTag, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+            var match = pattern.Match(raw);
+
+			while (match.Success)
+			{
+                if (match.Index > 0) targetText.Append(raw.Substring(0, match.Index));
+
+                // Remove the part before the found code block, and the code block, from the remaining 
+                // source text
+                raw = raw.Substring(match.Index + match.Length);
+
+                // Get the content of the found code block
+                string content = match.Value;
+
+                // The RegEx match still contains the opening and closing tags. Remove them so we get only the 
+                // text within the tag.
+                int openingTagLen = openingTag.Length;
+                int closingTagLen = closingTag.Length;
+                int contentLen = content.Length - closingTagLen - openingTagLen;
+                content = content.Substring(openingTagLen, contentLen);
+
+                if (!String.IsNullOrWhiteSpace(content))
+                {
+                    // Add an opening "pre" tag with a language ("brush") definition...
+                    targetText.AppendFormat(
+                        "<blockquote style=\"display: block; background: #F4F5F7; border: 1px dashed #CCC; margin: 5px 10px 10px 20px; padding: 8px 12px 8px 10px\"> \n");
+                    // ... the content...
+                    targetText.Append(content);
+                    // ... and a closing tag.
+                    targetText.Append("</blockquote>");
+                }
+
+			    // Get the next code block.
+                match = pattern.Match(raw);
+			}
+
+            // Append rest of source text to target.
+            targetText.Append(raw);
+
+			return targetText.ToString();
+		}
+
+		public string PrepareTitle(string title, ContextInformation context)
+		{
+			return title;
+		}
+
+		public void Init(IHostV30 host, string config)
+		{
+			_host = host;
+			_config = config ?? "";
+
+			if (_config.ToLowerInvariant() == "nolog") _enableLogging = false;
+		}
+
+		public void Shutdown()
+		{
+		}
+
+		public ComponentInformation Information
+		{
+			get
+			{
+				return new ComponentInformation("Wrapping Quote Block", "Chad Gorshing", "0.0.1", null, null);
+			}
+		}
+
+		public string ConfigHelpHtml
+		{
+			get { return "Specify <i>nolog</i> for disabling warning log messages for non-existent files or attachments."; }
+		}
+	}
+}
---
+++ WrappingQuoteBlockTests/BlockQuoteTests.cs
@@ -0,0 +1,130 @@
+﻿using BlockQuoteWithFormatting;
+using NUnit.Framework;
+using ScrewTurn.Wiki.PluginFramework;
+
+namespace WrappingBlockQuoteTests
+{
+    [TestFixture]
+    public class BlockQuoteTests
+    {
+        private BlockQuote _blockQuote;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _blockQuote = new BlockQuote();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        private ContextInformation BuildContext()
+        {
+            return new ContextInformation(false, false, FormattingContext.PageContent, null, null,
+                                                            null, null, null);
+        }
+
+        private string TagsWithContent(string data)
+        {
+            return "<blockquote style=\"display: block; background: #F4F5F7; border: 1px dashed #CCC; margin: 5px 10px 10px 20px; padding: 8px 12px 8px 10px\"> \n" + data + "</blockquote>";
+        }
+
+        [Test]
+        public void FormatDoesNothingWhenPhaseIsNotPageContent()
+        {
+            const string raw = "@@some text@@ and then shortly thereafter, ''some'' other tag(s)";
+
+            string result = _blockQuote.Format(raw, BuildContext(), FormattingPhase.Phase2);
+
+            Assert.That(result, Is.EqualTo(raw));
+        }
+
+        [Test]
+        public void FormatDoesNotAlterCopyWithOutTags()
+        {
+            const string raw = "some text";
+
+            string result = _blockQuote.Format(raw, BuildContext(), FormattingPhase.Phase2);
+
+            Assert.That(result, Is.EqualTo(raw));
+        }
+
+        [Test]
+        public void FormatDoesNotAlterCopyWithOtherTags()
+        {
+            const string raw = "@@some text@@";
+
+            string result = _blockQuote.Format(raw, BuildContext(), FormattingPhase.Phase2);
+
+            Assert.That(result, Is.EqualTo(raw));
+        }
+
+        [Test]
+        public void FormatDoesNotAlterCopyWithMultipleTags()
+        {
+            const string raw = "@@some text@@ and then shortly thereafter, ''some'' other tag(s)";
+
+            string result = _blockQuote.Format(raw, BuildContext(), FormattingPhase.Phase2);
+
+            Assert.That(result, Is.EqualTo(raw));
+        }
+
+        [Test]
+        public void FormatReplacesTagsWithNoContent()
+        {
+            const string raw = "{quote}{/quote}";
+
+            string result = _blockQuote.Format(raw, BuildContext(), FormattingPhase.Phase2);
+
+            Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public void FormatReplacesTagsWithSingleWordContent()
+        {
+            const string word = "Bocephus";
+            const string raw = "{quote}" + word + "{/quote}";
+
+            string result = _blockQuote.Format(raw, BuildContext(), FormattingPhase.Phase2);
+
+            Assert.That(result, Is.EqualTo(TagsWithContent(word)));
+        }
+
+        [Test]
+        public void FormatReplacesContentButNotOtherTags()
+        {
+            const string sentence = "''Bocephus is the man!''";
+            const string raw = "{quote}" + sentence + "{/quote}";
+
+            string result = _blockQuote.Format(raw, BuildContext(), FormattingPhase.Phase2);
+
+            Assert.That(result, Is.EqualTo(TagsWithContent(sentence)));
+        }
+
+        [Test]
+        public void FormatContinuesPastParagraphs()
+        {
+            const string paragraph =
+                "This will be on the first line\nThis will continue on to the second\nAnd finally the third or last line.";
+            const string raw = "{quote}" + paragraph + "{/quote}";
+
+            string result = _blockQuote.Format(raw, BuildContext(), FormattingPhase.Phase2);
+
+            Assert.That(result, Is.EqualTo(TagsWithContent(paragraph)));
+        }
+
+        [Test]
+        public void FormatHandlesDoubleTags()
+        {
+            const string raw = "Some lady said {quote}hello{/quote} then she said {quote}good bye{/quote}.";
+
+            string result = _blockQuote.Format(raw, BuildContext(), FormattingPhase.Phase2);
+
+            var shouldBe = "Some lady said " + TagsWithContent("hello") + " then she said " +
+                           TagsWithContent("good bye") + ".";
+            Assert.That(result, Is.EqualTo(shouldBe));
+        }
+    }
+}
