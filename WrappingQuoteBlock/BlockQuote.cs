794f9d7a5317493867cef45abfb4b70de8491014
Chad Gorshing <gorshing@gmail.com>
2010-08-02 23:16:39 -0500

    Found problems still arising from using the {quote} tag within either an ol or il list.

--- WrappingQuoteBlock/BlockQuote.cs
+++ WrappingQuoteBlock/BlockQuote.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Text.RegularExpressions;
 using ScrewTurn.Wiki.PluginFramework;
+using System.Collections.Generic;
 
 namespace BlockQuoteWithFormatting
 {
@@ -11,45 +12,47 @@ namespace BlockQuoteWithFormatting
     /// http://code.google.com/p/greeniciclescrewturnsyntaxhighlighter/source/browse/trunk/%20greeniciclescrewturnsyntaxhighlighter/src/SyntaxHighlightPlugin/SyntaxHighlightFormatProvider.cs
     /// I give them much props - thank you.
     /// </summary>
-	public class BlockQuote : IFormatterProviderV30
-	{
-		private IHostV30 _host;
-		private string _config;
-		private bool _enableLogging;
+    public class BlockQuote : IFormatterProviderV30
+    {
+        private const string LogName = "WrappingQuoteBlock";
+        private const int NotFound = -1;
+        private IHostV30 _host;
+        private string _config;
+        private bool _enableLogging;
 
-		public bool PerformPhase1
-		{
-			get { return false; }
-		}
+        public bool PerformPhase1
+        {
+            get { return false; }
+        }
 
-		public bool PerformPhase2
-		{
-			get { return true; }
-		}
+        public bool PerformPhase2
+        {
+            get { return true; }
+        }
 
-		public bool PerformPhase3
-		{
-			get { return false; }
-		}
+        public bool PerformPhase3
+        {
+            get { return false; }
+        }
 
-		public int ExecutionPriority
-		{
-			get { return 50; }
-		}
+        public int ExecutionPriority
+        {
+            get { return 50; }
+        }
 
-		public string Format(string raw, ContextInformation context, FormattingPhase phase)
-		{
+        public string Format(string raw, ContextInformation context, FormattingPhase phase)
+        {
             if (context.Context != FormattingContext.PageContent) return raw;
 
             var targetText = new StringBuilder();
-		    string openingTag = "{quote}";
+            string openingTag = "{quote}";
             string closingTag = "{/quote}";
             var pattern = new Regex(openingTag + ".*?" + closingTag, RegexOptions.IgnoreCase | RegexOptions.Singleline);
 
             var match = pattern.Match(raw);
 
-			while (match.Success)
-			{
+            while (match.Success)
+            {
                 if (match.Index > 0) targetText.Append(raw.Substring(0, match.Index));
 
                 // Remove the part before the found code block, and the code block, from the remaining 
@@ -68,6 +71,8 @@ namespace BlockQuoteWithFormatting
 
                 if (!String.IsNullOrWhiteSpace(content))
                 {
+                    var closings = FindAnySingleClosingTags(ref content);
+
                     // Add an opening "pre" tag with a language ("brush") definition...
                     targetText.AppendFormat(
                         "<blockquote style=\"display: block; background: #F4F5F7; border: 1px dashed #CCC; margin: 5px 10px 10px 20px; padding: 8px 12px 8px 10px\"> \n");
@@ -75,46 +80,137 @@ namespace BlockQuoteWithFormatting
                     targetText.Append(content);
                     // ... and a closing tag.
                     targetText.Append("</blockquote>");
+
+                    closings.ForEach(x =>
+                        {
+                            targetText.Append(x);
+                        });
                 }
 
-			    // Get the next code block.
+                // Get the next code block.
                 match = pattern.Match(raw);
-			}
+            }
 
             // Append rest of source text to target.
             targetText.Append(raw);
 
-			return targetText.ToString();
-		}
+            return targetText.ToString();
+        }
 
-		public string PrepareTitle(string title, ContextInformation context)
-		{
-			return title;
-		}
+        public List<string> FindAnySingleClosingTags(ref string content)
+        {
+            var result = new List<string>();
+            var seenBefore = new List<string>();
 
-		public void Init(IHostV30 host, string config)
-		{
-			_host = host;
-			_config = config ?? "";
+            if (content.IndexOf("quote}") != NotFound)
+                _host.LogEntry("We should not be finding *quote} in here.", LogEntryType.Warning, LogName, this);
 
-			if (_config.ToLowerInvariant() == "nolog") _enableLogging = false;
-		}
+            int i = 0;
+            while (i < content.Length)
+            {
+                var lookingAt = content.Substring(i);
 
-		public void Shutdown()
-		{
-		}
+                var closingTag = "</";
+                if (lookingAt.StartsWith(closingTag))
+                {
+                    var ending = IndexOfClosingBracket(lookingAt);
 
-		public ComponentInformation Information
-		{
-			get
-			{
-				return new ComponentInformation("Wrapping Quote Block", "Chad Gorshing", "0.0.1", null, null);
-			}
-		}
+                    if (ending != NotFound)
+                    {
+                        var tagName = lookingAt.Substring(closingTag.Length, ending - closingTag.Length);
 
-		public string ConfigHelpHtml
-		{
-			get { return "Specify <i>nolog</i> for disabling warning log messages for non-existent files or attachments."; }
-		}
-	}
+                        if (seenBefore.Count > 0 && seenBefore[0] == tagName) seenBefore.RemoveAt(0);
+                        else
+                        {
+                            _host.LogEntry("Found a closing tag of " + tagName, LogEntryType.General, LogName, this);
+                            result.Add(string.Format("</{0}>", tagName));
+
+                            //1 is for including the closing bracket
+                            content = content.Remove(i, ending+1);
+                            continue;
+                        }
+                    }
+                }
+                else if (lookingAt.StartsWith("<"))
+                {
+                    int ending = 0;
+
+                    var spaceEnd = lookingAt.IndexOf(' ');
+                    int bracketEnd = IndexOfClosingBracket(lookingAt);
+
+                    if (bracketEnd != NotFound && NextIsNotSpace(spaceEnd))
+                    {
+                        if (spaceEnd == NotFound) ending = bracketEnd;
+                        else if (spaceEnd < bracketEnd) ending = spaceEnd;
+                        else ending = bracketEnd;
+
+                        //The 1 is to skip over the opening bracket,
+                        //The -1 is to offset the 1
+                        var tagName = lookingAt.Substring(1, ending - 1);
+
+                        if (!SelfClosingTag(lookingAt, bracketEnd))
+                            seenBefore.Insert(0, tagName); //synonymous to a push
+
+                        i += MoveToEndOfBracket(bracketEnd);
+                    }
+                }
+
+                ++i;
+            }
+
+            return result;
+        }
+
+        private static bool NextIsNotSpace(int spaceEnd)
+        {
+            return spaceEnd == -1 || spaceEnd > 1;
+        }
+
+        private static int IndexOfClosingBracket(string lookingAt)
+        {
+            return lookingAt.IndexOf('>');
+        }
+
+        private int MoveToEndOfBracket(int bracketEnd)
+        {
+            return bracketEnd;
+        }
+
+        private bool SelfClosingTag(string content, int bracketEnd)
+        {
+            //bracketEnd should be the index of the closing bracket
+            //so just go back one spot
+            return content[bracketEnd - 1] == '/';
+        }
+
+        public string PrepareTitle(string title, ContextInformation context)
+        {
+            return title;
+        }
+
+        public void Init(IHostV30 host, string config)
+        {
+            _host = host;
+            _config = config ?? "";
+
+            if (_config.ToLowerInvariant() == "nolog") _enableLogging = false;
+        }
+
+        public void Shutdown()
+        {
+        }
+
+        public ComponentInformation Information
+        {
+            get
+            {
+                return new ComponentInformation("Wrapping Quote Block", "Chad Gorshing", "0.0.1", null, null);
+            }
+        }
+
+        public string ConfigHelpHtml
+        {
+            get { return "Specify <i>nolog</i> for disabling warning log messages for non-existent files or attachments."; }
+        }
+    }
 }
--- WrappingQuoteBlockTests/BlockQuoteTests.cs
+++ WrappingQuoteBlockTests/BlockQuoteTests.cs
@@ -1,6 +1,7 @@
 ﻿using BlockQuoteWithFormatting;
 using NUnit.Framework;
 using ScrewTurn.Wiki.PluginFramework;
+using Rhino.Mocks;
 
 namespace WrappingQuoteBlockTests
 {
@@ -8,11 +9,19 @@ namespace WrappingQuoteBlockTests
     public class BlockQuoteTests
     {
         private BlockQuote _blockQuote;
+        private MockRepository _mocks;
+        private IHostV30 _mockedHost;
 
         [SetUp]
         public void SetUp()
         {
+            _mocks = new MockRepository();
+
+            _mockedHost = _mocks.DynamicMock<IHostV30>();
+
             _blockQuote = new BlockQuote();
+
+            _blockQuote.Init(_mockedHost, string.Empty);
         }
 
         [TearDown]
@@ -36,7 +45,7 @@ namespace WrappingQuoteBlockTests
         {
             const string raw = "@@some text@@ and then shortly thereafter, ''some'' other tag(s)";
 
-            string result = _blockQuote.Format(raw, BuildContext(), FormattingPhase.Phase2);
+            string result = CallFormat(raw);
 
             Assert.That(result, Is.EqualTo(raw));
         }
@@ -46,7 +55,7 @@ namespace WrappingQuoteBlockTests
         {
             const string raw = "some text";
 
-            string result = _blockQuote.Format(raw, BuildContext(), FormattingPhase.Phase2);
+            string result = CallFormat(raw);
 
             Assert.That(result, Is.EqualTo(raw));
         }
@@ -56,7 +65,7 @@ namespace WrappingQuoteBlockTests
         {
             const string raw = "@@some text@@";
 
-            string result = _blockQuote.Format(raw, BuildContext(), FormattingPhase.Phase2);
+            string result = CallFormat(raw);
 
             Assert.That(result, Is.EqualTo(raw));
         }
@@ -66,7 +75,7 @@ namespace WrappingQuoteBlockTests
         {
             const string raw = "@@some text@@ and then shortly thereafter, ''some'' other tag(s)";
 
-            string result = _blockQuote.Format(raw, BuildContext(), FormattingPhase.Phase2);
+            string result = CallFormat(raw);
 
             Assert.That(result, Is.EqualTo(raw));
         }
@@ -76,7 +85,7 @@ namespace WrappingQuoteBlockTests
         {
             const string raw = "{quote}{/quote}";
 
-            string result = _blockQuote.Format(raw, BuildContext(), FormattingPhase.Phase2);
+            string result = CallFormat(raw);
 
             Assert.That(result, Is.Empty);
         }
@@ -87,7 +96,7 @@ namespace WrappingQuoteBlockTests
             const string word = "Bocephus";
             const string raw = "{quote}" + word + "{/quote}";
 
-            string result = _blockQuote.Format(raw, BuildContext(), FormattingPhase.Phase2);
+            string result = CallFormat(raw);
 
             Assert.That(result, Is.EqualTo(TagsWithContent(word)));
         }
@@ -98,7 +107,7 @@ namespace WrappingQuoteBlockTests
             const string sentence = "''Bocephus is the man!''";
             const string raw = "{quote}" + sentence + "{/quote}";
 
-            string result = _blockQuote.Format(raw, BuildContext(), FormattingPhase.Phase2);
+            string result = CallFormat(raw);
 
             Assert.That(result, Is.EqualTo(TagsWithContent(sentence)));
         }
@@ -110,7 +119,7 @@ namespace WrappingQuoteBlockTests
                 "This will be on the first line\nThis will continue on to the second\nAnd finally the third or last line.";
             const string raw = "{quote}" + paragraph + "{/quote}";
 
-            string result = _blockQuote.Format(raw, BuildContext(), FormattingPhase.Phase2);
+            string result = CallFormat(raw);
 
             Assert.That(result, Is.EqualTo(TagsWithContent(paragraph)));
         }
@@ -120,11 +129,160 @@ namespace WrappingQuoteBlockTests
         {
             const string raw = "Some lady said {quote}hello{/quote} then she said {quote}good bye{/quote}.";
 
-            string result = _blockQuote.Format(raw, BuildContext(), FormattingPhase.Phase2);
+            string result = CallFormat(raw);
 
             var shouldBe = "Some lady said " + TagsWithContent("hello") + " then she said " +
                            TagsWithContent("good bye") + ".";
             Assert.That(result, Is.EqualTo(shouldBe));
+        }
+
+        [Test]
+        public void FormatHandlesBrokenApartTags()
+        {
+            //string raw = "* One\n{quote}foo\n\nfoo2\n{/quote}";
+            //string raw = "<ul><li>One<br />" + TagsWithContent("foo<br /></li></ul><br />foo2");
+            string raw = "<ul><li>One<br />{quote}foo<br /></li></ul><br />foo2 {/quote}";
+
+            string result = CallFormat(raw);
+
+            var shouldBe = "<ul><li>One<br />" + TagsWithContent("foo<br /><br />foo2 ") + "</li></ul>";
+            Assert.That(result, Is.EqualTo(shouldBe));
+        }
+
+        [Test]
+        public void FormatMovesToEnd()
+        {
+            string raw = "{quote}Some</b> lady{/quote}";
+
+            string result = CallFormat(raw);
+
+            Assert.That(result, Is.EqualTo(TagsWithContent("Some lady") + "</b>"));
+        }
+
+        [Test]
+        public void FormatDoesNothingWhenTagsMatchUp()
+        {
+            string raw = "{quote}Some <b>lady</b>{/quote}";
+
+            string result = CallFormat(raw);
+
+            Assert.That(result, Is.EqualTo(TagsWithContent("Some <b>lady</b>")));
+        }
+
+        [Test]
+        public void FormatDoesNothingWhenNestedCorrectly()
+        {
+            string raw = "{quote}Some <b>la<i>d</i>y</b>{/quote}";
+
+            string result = CallFormat(raw);
+
+            Assert.That(result, Is.EqualTo(TagsWithContent("Some <b>la<i>d</i>y</b>")));
+        }
+
+        [Test]
+        public void FormatHandlesAttributes()
+        {
+            string raw = "{quote}Some <a href=\"foo.html\" id=\"x9\">lady</a>{/quote}";
+
+            string result = CallFormat(raw);
+
+            Assert.That(result, Is.EqualTo(TagsWithContent("Some <a href=\"foo.html\" id=\"x9\">lady</a>")));
+        }
+
+        [Test]
+        public void FormatDoesNothingWithLessThan()
+        {
+            string raw = "{quote}Did you know that 1 < 2 == true?{/quote}";
+
+            string result = CallFormat(raw);
+
+            Assert.That(result, Is.EqualTo(TagsWithContent("Did you know that 1 < 2 == true?")));
+        }
+
+        [Test]
+        public void FormatDoesNothingWithHalfClosingTag()
+        {
+            string raw = "{quote}foo </{/quote}";
+
+            string result = CallFormat(raw);
+
+            Assert.That(result, Is.EqualTo(TagsWithContent("foo </")));
+        }
+
+        [Test]
+        public void FormatDoesNothingWithHalfOpenedTag()
+        {
+            string raw = "{quote}foo <b{/quote}";
+
+            string result = CallFormat(raw);
+
+            Assert.That(result, Is.EqualTo(TagsWithContent("foo <b")));
+        }
+
+        [Test]
+        public void FormatDoesNothingWithSpacedOutOpeningTag()
+        {
+            string raw = "{quote}foo <div class=\"nav\" {/quote}";
+
+            string result = CallFormat(raw);
+
+            Assert.That(result, Is.EqualTo(TagsWithContent("foo <div class=\"nav\" ")));
+        }
+
+        [Test]
+        public void Foo()
+        {
+            string raw = "{quote}foo <div> {/quote}";
+
+            string result = CallFormat(raw);
+
+            Assert.That(result, Is.EqualTo(TagsWithContent("foo <div> ")));
+        }
+
+        [Test]
+        public void FormatDoesNothingWithHalfOpenedTag2()
+        {
+            string raw = "{quote}foo <b {/quote}";
+
+            string result = CallFormat(raw);
+
+            Assert.That(result, Is.EqualTo(TagsWithContent("foo <b ")));
+        }
+
+        [Test]
+        public void FormatHandlesSelfClosingTags()
+        {
+            string raw = "{quote}foo<br /></li></ul><br />foo2 {/quote}";
+
+            string result = CallFormat(raw);
+
+            Assert.That(result, Is.EqualTo(TagsWithContent("foo<br /><br />foo2 ") + "</li></ul>"));
+        }
+
+        [Test]
+        public void FormatDoesNothingWithHtmlComments()
+        {
+            string raw = "{quote}foo <!-- shhhh -->{/quote}";
+
+            string result = CallFormat(raw);
+
+            Assert.That(result, Is.EqualTo(TagsWithContent("foo <!-- shhhh -->")));
+        }
+
+        [Ignore]
+        [Test]
+        public void Format______WhatToDoAboutBrokenHtmlComments()
+        {
+            string raw = "{quote}foo <!-- shhhh{/quote} another -->";
+
+            string result = CallFormat(raw);
+
+            Assert.That(result, Is.EqualTo("Fix this, I'm not sure what to do"));
+        }
+
+        private string CallFormat(string data)
+        {
+            return _blockQuote.Format(data, BuildContext(), FormattingPhase.Phase2);
         }
     }
 }
